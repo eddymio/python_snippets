4fd3bd21b3ac2c996496ea88bea4fa206a202966
Eddy Mio <you@mip.com>
2020-07-16 21:56:46 +0200

    Add positional argument to program - content

--- fileIO/main.py
+++ fileIO/main.py
@@ -3,17 +3,18 @@ import argparse
 from fileIO.BulkFileModification import BulkFileModification
 
 
-def main(path, extension):
+def main(path, extension, content):
     bulk = BulkFileModification(path, extension)
-    bulk.bulk_head_insert('<script>')
     print('Operating on the following files : ', bulk.file_paths)
+    bulk.bulk_head_insert(content)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', help='Indicate the absolute path of the files to browse.', default='.')
     parser.add_argument('--ext', help='Provide the extension to filter with.', default='html')
+    parser.add_argument('content', help='Content to include (ex : "<script>...</script>").')
     args = parser.parse_args()
 
     # execute only if run as a script
-    main(args.path, args.ext)
+    main(args.path, args.ext, args.content)
