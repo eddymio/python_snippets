55a334667b4018fe2f89fb95386f27861d02899c
Eddy Mio <you@mip.com>
2020-07-19 09:19:31 +0200

    Improve the file I/O implementation

--- fileIO/FileModification.py
+++ fileIO/FileModification.py
@@ -5,15 +5,13 @@ class FileModification:
 
     def __init__(self, file_path):
         self._file_path = file_path
-        self._file = open(self._file_path, "r")
+        self._file = open(self._file_path, "r+")
         self.content = self._file.read()
-        self._file.close()
 
     def head_append(self, newline):
         index = self.content.index('</head>')
         self.content = self.content[0:index] + newline + self.content[index:]
 
     def write(self):
-        self._file = open(self._file_path, "w")
         self._file.write(self.content)
         self._file.close()
